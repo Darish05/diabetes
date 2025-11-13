a53899aa2f14c99b58102597dafc9155ade3820e
Darish05 <darishjerlin2005@gmail.com>
2025-11-13 12:45:31 +0530

    updated

--- train.py
+++ train.py
@@ -1,21 +1,20 @@
-import pandas as pd 
+import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import joblib
 
-data=pd.read_csv('diabetes.csv')
-x=data[['Glucose','BloodPressure','Insulin','BMI','DiabetesPedigreeFunction','Age']]
-y=data['Outcome']
+data = pd.read_csv('diabetes.csv')
+X = data[['Glucose', 'Insulin', 'BMI']]
+y = data['Outcome']
 
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
 
-model=LinearRegression()
-model.fit(x_train,y_train)
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+print(f"Model Accuracy: {acc:.2f}")
 
-y_pred=model.predict(x_test)
-acc=accuracy_score(y_test,y_pred)
-print("Accuracy:",acc)
-
-joblib.dump((model,acc),'linear_model.pkl')
-print("model and accuracy saved in linear_model.pkl")
+joblib.dump((model, acc), 'forest_model.pkl')
+print("Model and accuracy saved in forest_model.pkl")
